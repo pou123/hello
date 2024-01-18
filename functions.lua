508e557b5e4c47ab955209063fe679ea507798fa
pou123 <102804520+pou123@users.noreply.github.com>
2024-01-19 00:32:05 +0100

    Add files via upload

--- functions.lua
+++ functions.lua
@@ -1,7 +1,12 @@
 {
   {
     "chat",
+    "Creator",
+    "function trim(s) return s:match'^()%s*$' and '' or s:match'^%s*(.*%S)' end local file = fs.open('Variables', 'r') local contents = file.readAll() file.close() contents = textutils.unserialize(contents) for i,v in pairs(contents.WhitelistPlayers) do if string.lower(p1) == string.lower(v) and string.lower(p2):sub(1, #contents.Call) == string.lower(contents.Call) then  local cont = trim(p2:sub(#contents.Call + 1)) forcefuncr('test') end end",
+  },
+  {
+    "nil",
     "test",
-    "print(p2) if p2 == 'clear' then os.clear() end",
+    "print('test completed')",
   },
 }
